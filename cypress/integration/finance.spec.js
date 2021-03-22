37ad8c687d8649c7451f6e55eead6206089a87eb
Wenderson Monteiro <49308695+wenderson-me@users.noreply.github.com>
2021-03-22 17:10:18 -0300

    cadastro sainda

--- cypress/integration/finance.spec.js
+++ cypress/integration/finance.spec.js
@@ -8,14 +8,26 @@ context('Dev Finances', () => {
 
     cy.visit('https://devfinance-agilizei.netlify.app')
 
+    cy.get('#data-table tbody tr').should('have.length', 0) // tabela vazia
+
     cy.get('#transaction .button').click() //id + classe
     cy.get('#description').type('Presente') //id
-    cy.get('[name=amount]').type(12) //atributo...
+    cy.get('[name=amount]').type(30) //atributo...
     cy.get('[type=date]').type('2021-03-21') //.atributos
     cy.get('button').contains('Salvar').click() //tipo e valor
   
   });
 
   // Cadastrar saidas
+  it('Cadastrar saidas', () => {
+
+    cy.get('#data-table tbody tr').should('have.length', 1)
+
+    cy.get('#transaction .button').click()
+    cy.get('#description').type('Presente')
+    cy.get('[name=amount]').type(-30)
+    cy.get('[type=date]').type('2021-03-30')
+    cy.get('button').contains('Salvar').click()
+  })
   // Remover entradas e saidas
 })
